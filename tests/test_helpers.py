a7e8c133f42cd32d5628f1b1dac643d1c2b79a36
dima borisov <dim4ig2007@gmail.com>
2026-05-02 01:17:30 +0500

    Delete type specification

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -11,26 +11,26 @@ class TestDictInitializer():
         return LZWArchiver()
 
     @pytest.mark.parametrize("mode", [DictMode.ENCODE, DictMode.DECODE])
-    def test_init_dict_size(self, archiver: LZWArchiver, mode):
+    def test_init_dict_size(self, archiver, mode):
         dictionary = archiver._init_dict(mode)
         assert len(dictionary) == 256
 
-    def test_init_dict_encode_values(self, archiver: LZWArchiver):
+    def test_init_dict_encode_values(self, archiver):
         dictionary = archiver._init_dict(DictMode.ENCODE)
         for i, b in enumerate(dictionary):
             assert i == dictionary[b]
 
-    def test_init_dict_decode_values(self, archiver: LZWArchiver):
+    def test_init_dict_decode_values(self, archiver):
         dictionary = archiver._init_dict(DictMode.DECODE)
         for i, b in enumerate(dictionary):
             assert bytes([i]) == dictionary[b]
 
-    def test_init_dict_incorrect_mode(self, archiver: LZWArchiver):
+    def test_init_dict_incorrect_mode(self, archiver):
         with pytest.raises(ValueError) as excinfo:
             archiver._init_dict('FOO')
         assert "Invalid mode" in str(excinfo.value)
 
-    def test_init_dict_creates_new_object(self, archiver: LZWArchiver):
+    def test_init_dict_creates_new_object(self, archiver):
         dict1 = archiver._init_dict(DictMode.ENCODE)
         dict2 = archiver._init_dict(DictMode.ENCODE)
         assert dict1 is not dict2
