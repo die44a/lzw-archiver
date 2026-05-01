b1543f5dec7cabaf522d9da7c706c513caffd079
dima borisov <dim4ig2007@gmail.com>
2026-05-01 22:51:43 +0500

    Add autopep8 integration

--- logic/lzw.py
+++ logic/lzw.py
@@ -1,19 +1,15 @@
-"""
-A class that implements archiving and unarchiving 
-    using the LZW algorithm
-"""
-
 from enum import Enum
 from pathlib import Path
 from typing import BinaryIO, Iterator
 from .constants import (
-    DICT_SIZE_LIMIT, 
-    DEFAULT_MAX_DICT, 
-    BYTE_ORDER, 
+    DICT_SIZE_LIMIT,
+    DEFAULT_MAX_DICT,
+    BYTE_ORDER,
     CODE_SIZE
 )
 
 __all__ = ['LZWArchiver']
+
 
 class DictMode(Enum):
     """
@@ -23,52 +19,57 @@ class DictMode(Enum):
     ENCODE = 1
     DECODE = 2
 
+
 class LZWArchiver():
+    """The class implementing LZM archiver"""
+
     def __init__(self, max_dict_size=DEFAULT_MAX_DICT):
         self.max_dict_size = min(max_dict_size, DICT_SIZE_LIMIT)
-            
-    
-    def encode(self, input_path : str | Path, output_path: str | Path = None) -> None:
+
+    def encode(self, input_path: str | Path,
+               output_path: str | Path = None) -> None:
         """Encodes file using LZW algorithm"""
-        
         input_path = Path(input_path)
-        
+
         if not input_path.exists():
             raise FileNotFoundError(f"Input file doesn't exist: {input_path}")
-        
-        output_path = Path(output_path or input_path.with_suffix(input_path.suffix + ".lzw"))
-                
-        #header = b'LZW' + self.max_dict_size.to_bytes(2, self.BYTE_ORDER)  # TODO: write down file header
-        
-        with open(input_path, 'rb') as input_file, open(output_path, 'wb') as output_file:
+
+        output_path = Path(output_path or
+                           input_path.with_suffix(input_path.suffix + ".lzw"))
+
+        # header = b'LZW' + self.max_dict_size.to_bytes(2, self.BYTE_ORDER)  # TODO: write down file header
+
+        with (open(input_path, 'rb') as input_file,
+              open(output_path, 'wb') as output_file):
             codes = self._bytes_to_codes(self._read_bytes(input_file))
 
             for code in codes:
                 output_file.write(code.to_bytes(CODE_SIZE, BYTE_ORDER))
-            
-            
-    def decode(self, input_path : str | Path, output_path: str | Path = None) -> None:
+
+    def decode(self, input_path: str | Path,
+               output_path: str | Path = None) -> None:
         """Decodes file encoded by LZW algorithm"""
         input_path = Path(input_path)
-        
+
         if not input_path.exists():
             raise FileNotFoundError(f"Input file doesn't exist: {input_path}")
-        
+
         if input_path.suffix != '.lzw':
             raise ValueError(f"File {input_path} must have .lzw extension")
-        
+
         if not output_path:
-            original_name = input_path.stem 
+            original_name = input_path.stem
             output_path = input_path.parent / f"decoded_{original_name}"
-        
-        with open(input_path, 'rb') as input_file, open(output_path, 'wb') as output_file:
+
+        with (open(input_path, 'rb') as input_file,
+              open(output_path, 'wb') as output_file):
             bytes = self._codes_to_bytes(self._read_codes(input_file))
 
             for byte in bytes:
                 output_file.write(byte)
-                
-    @staticmethod            
-    def _init_dict(mode : DictMode) -> dict[bytes, int] | dict[int, bytes]:
+
+    @staticmethod
+    def _init_dict(mode: DictMode) -> dict[bytes, int] | dict[int, bytes]:
         """Initializes dictionary for encoding and decoding based on mode"""
         if mode == DictMode.ENCODE:
             return {bytes([i]): i for i in range(256)}
@@ -76,72 +77,70 @@ class LZWArchiver():
             return {i: bytes([i]) for i in range(256)}
         else:
             raise ValueError("Invalid mode")
-                
+
     @staticmethod
-    def _read_bytes(input_file : BinaryIO) -> Iterator[bytes]:
+    def _read_bytes(input_file: BinaryIO) -> Iterator[bytes]:
         """Reads bytes from the file one by one"""
-        while(byte := input_file.read(1)):
+        while (byte := input_file.read(1)):
             yield byte
-    
-    
-    def _bytes_to_codes(self, bytes_iter : Iterator[bytes]) -> Iterator[int]:
+
+    def _bytes_to_codes(self, bytes_iter: Iterator[bytes]) -> Iterator[int]:
         """Transforms bytes sequence into codes sequence"""
         dictionary = self._init_dict(DictMode.ENCODE)
         next_code = 256
         prefix = b''
-        
-        for byte in bytes_iter:    
+
+        for byte in bytes_iter:
             pc = prefix + byte
             if pc in dictionary:
                 prefix = pc
                 continue
-                
+
             yield dictionary[prefix]
-            
+
             if next_code < self.max_dict_size:
                 dictionary[pc] = next_code
                 next_code += 1
-                
+
             prefix = byte
-        
+
         if prefix:
             yield dictionary[prefix]
-            
-    @staticmethod    
-    def _read_codes(input_file : BinaryIO) -> Iterator[int]:
+
+    @staticmethod
+    def _read_codes(input_file: BinaryIO) -> Iterator[int]:
         """Reads codes from the file (each code takes CODE_SIZE in te file)"""
-        while(code := input_file.read(CODE_SIZE)):
+        while (code := input_file.read(CODE_SIZE)):
             yield int.from_bytes(code, BYTE_ORDER)
-            
-    
-    def _codes_to_bytes(self, codes_iter : Iterator[int]) -> Iterator[bytes]:
+
+    def _codes_to_bytes(self, codes_iter: Iterator[int]) -> Iterator[bytes]:
         """Transforms codes sequence into bytes sequence"""
         codes_iter = iter(codes_iter)
         try:
             first_code = next(codes_iter)
         except StopIteration:
-            return 
-        
+            return
+
         dictionary = self._init_dict(DictMode.DECODE)
         next_code = 256
         sequence = dictionary[first_code]
         yield sequence
-        
+
         for code in codes_iter:
             if code in dictionary:
                 entry = dictionary[code]
-            elif code == next_code:  
+            elif code == next_code:
                 # Rare case when encoder havnt added sequence yet
-                # For examole 'aaaaa' encoded output should be ['97', '256', '256']     
-                # This only happens if the sequence starts and ends with the same symbol       
+                # For examole 'aaaaa' encoded output should be ['97', '256', '256']
+                # This only happens if the sequence starts and ends with the same symbol
                 entry = sequence + sequence[:1]
             else:
                 raise ValueError(f"Bad compressed code: {code}")
-            
+
             yield entry
-            
+
             if next_code < self.max_dict_size:
                 dictionary[next_code] = sequence + entry[:1]
                 next_code += 1
-            
-            sequence = entry   
+
+            sequence = entry
