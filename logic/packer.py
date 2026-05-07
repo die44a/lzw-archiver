ce26d5494ee833058d133be0aeb5c21f41c2cf29
dima borisov <dim4ig2007@gmail.com>
2026-05-07 19:08:35 +0500

    Add files/directories packer

---
+++ logic/packer.py
@@ -0,0 +1,28 @@
+import tarfile
+from pathlib import Path
+
+
+class TarPacker:
+    """The class implementing packing directory into one file 
+    using tarfile extention (way of packing may be changed if needed)"""
+
+    def __init__(self):
+        pass
+
+    def pack(self, input_path: str | Path, output_path: str | Path) -> Path:
+        input_path = Path(input_path).resolve()
+        output_path = Path(output_path).resolve().with_suffix('.tar')
+
+        with tarfile.open(output_path, 'w') as tar:
+            tar.add(input_path, arcname=input_path.name)
+
+        return output_path
+
+    def unpack(self, input_path: str | Path, output_path: str | Path) -> Path:
+        input_path = Path(input_path).resolve()
+        output_path = Path(output_path).resolve()
+
+        with tarfile.open(input_path, 'r') as tar:
+            tar.extractall(path=output_path, filter="data")
+
+        return output_path
