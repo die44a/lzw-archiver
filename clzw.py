b3c23a8e8e7d2f0723583f0b1565197921440335
dima borisov <dim4ig2007@gmail.com>
2026-05-07 12:28:16 +0500

    Add arguments parsing

--- clzw.py
+++ clzw.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """Console version of LZW archiver"""
 
 
@@ -8,6 +10,7 @@ import inspect
 import enum
 import argparse
 import sys
+
 
 ERROR_EXCEPTION = 1
 ERROR_PYTHON_VERSION = 2
@@ -31,3 +34,51 @@ except Exception as e:
 __version__ = '0.1'
 __author__ = 'Dima Borisov'
 __email__ = 'dim4ig2007@gmail.com'
+
+
+def parse_args():
+    """Command line arguments parsing"""
+    parser = argparse.ArgumentParser(
+        prog='LZW-Archiver',
+        description=f'Compresses or extracts files from specified file or directory',
+        epilog=f'Author: {__author__} <{__email__}>')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    parser.add_argument(
+        '-V', '--version',
+        action='version',
+        version=f'%(prog)s {__version__}')
+
+    group.add_argument(
+        '-x', '--extract',
+        action='store_true',
+        help='extract file or directory')
+
+    group.add_argument(
+        '-c', '--compress',
+        action='store_true',
+        help='compress file or directory')
+
+    parser.add_argument('PATH', type=str,
+                        help='path to the file or directory')
+
+    parser.add_argument(
+        '-o', '--output', type=str,
+        metavar='OUTPUT',
+        help='path to the output directory')
+
+    parser.add_argument(
+        '-f', '--force',
+        action='store_true',
+        help='rewrite existing output directory if exists')
+
+    return parser.parse_args()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
