9d675e0364d0272af06d457aab24287d05ebe4fe
dima borisov <dim4ig2007@gmail.com>
2026-05-16 01:03:18 +0500

    Rewrite packer logic

--- logic/packer.py
+++ logic/packer.py
@@ -1,5 +1,8 @@
-import tarfile
 from pathlib import Path
+from typing import BinaryIO
+
+PATH_SIZE = 256
+FIlE_SIZE_FIELD_SIZE = 8
 
 
 class TarPacker:
@@ -24,11 +27,39 @@ class TarPacker:
         input_path = Path(input_path).resolve()
         output_path = Path(
             output_path or input_path.with_suffix('.tar')).resolve()
+        if output_path.suffix != '.tar':
+            raise ValueError(f"File {output_path} must have .tar extension")
 
-        with tarfile.open(output_path, 'w') as tar:
-            tar.add(input_path, arcname=input_path.name)
+        with open(output_path, 'wb') as tar:
+            if input_path.is_file():
+                self._write_file_to_archive(input_path, input_path, tar)
+                return output_path
+
+            for item in input_path.rglob('*'):
+                if item.is_file():
+                    self._write_file_to_archive(item, input_path.parent, tar)
 
         return output_path
+
+    def _write_file_to_archive(self, file_path: Path, base_path: Path, archive_stream: BinaryIO):
+        path_bt = bytes(
+            str(file_path.relative_to(base_path)), encoding='utf-8')
+        if len(path_bt) > PATH_SIZE:
+            raise ValueError(
+                f'File name size must me less or equal to {PATH_SIZE}')
+        path_bt = path_bt.ljust(PATH_SIZE, b'\x00')
+
+        file_size_bt = bytes(str(file_path.stat().st_size),
+                             encoding='utf-8')
+        if len(file_size_bt) > FIlE_SIZE_FIELD_SIZE:
+            raise ValueError(
+                f'file size must be less or equal to {FIlE_SIZE_FIELD_SIZE}')
+        file_size_bt = file_size_bt.ljust(FIlE_SIZE_FIELD_SIZE, b'\x00')
+
+        archive_stream.write(path_bt)
+        archive_stream.write(file_size_bt)
+        with open(file_path, 'rb') as f_in:
+            archive_stream.write(f_in.read())
 
     def unpack(self, input_path: str | Path, output_path: str | Path = None) -> Path:
         """
@@ -43,9 +74,32 @@ class TarPacker:
             Path: The path to the directory where files were extracted.
         """
         input_path = Path(input_path).resolve()
+        if input_path.suffix != '.tar':
+            raise ValueError(f"File {input_path} must have .tar extension")
         output_path = Path(output_path or input_path.stem).resolve()
 
-        with tarfile.open(input_path, 'r') as tar:
-            tar.extractall(path=output_path, filter='data')
+        with open(input_path, 'rb') as tar:
+            while True:
+                if not self._create_file(output_path, tar):
+                    break
 
-            return output_path
+        return output_path
+
+    def _create_file(self, output: Path, archive_stream: BinaryIO):
+        raw_path = archive_stream.read(PATH_SIZE)
+        if not raw_path:
+            return False
+
+        file_path = raw_path.decode('utf-8').rstrip('\x00')
+
+        file_size = int(archive_stream.read(
+            FIlE_SIZE_FIELD_SIZE).decode('utf-8').rstrip('\x00'))
+
+        final_path = output / file_path
+
+        final_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with open(final_path, 'wb') as f:
+            f.write(archive_stream.read(file_size))
+
+        return True
